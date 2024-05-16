57e0ba0394e27b04ea58504e8308c75263a2b6d3
Tomeu <tmora307@alumnes.politecnicllevant.cat>
2024-05-16 11:29:19 +0200

    Creacio del menu de joc i canvis menors

--- Rummy/src/Game.java
+++ Rummy/src/Game.java
@@ -1,6 +1,25 @@
 public class Game {
     private static Menu menu = new Menu();
-    public static void jugar(){
-        menu.elecio();
+
+    public static int eleccio(){
+
+        switch (menu.elecio()){
+            case 1:
+                return 1;
+            case 2:
+                return 2;
+            case 3:
+                return 3;
+            case 4:
+                return 4;
+            default :
+                Missatge.soutText("Unexpected tocken");
+                throw new IllegalStateException("Unexpected value: " + menu.elecio());
+
+        }
+    }
+
+    public Game(int num){
+
     }
 }
--- Rummy/src/Main.java
+++ Rummy/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Game.jugar();
+        Game joc = new Game(Game.eleccio());
     }
 }
