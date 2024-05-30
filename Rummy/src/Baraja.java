9a44f6902ae20c58379c3425f0ef8c5acd18249e
Tomeu <tmora307@alumnes.politecnicllevant.cat>
2024-05-30 10:22:38 +0200

    Creacio dels jokers

--- Rummy/src/Baraja.java
+++ Rummy/src/Baraja.java
@@ -5,7 +5,7 @@ public class Baraja {
     private ArrayList<Fitxa> cartes;
 
     public Baraja(int num ) {
-        cartes = new ArrayList<>();
+        cartes = new ArrayList<Fitxa>();
         if (num == 1) {
             String[] palos = {"Corazones", "Diamantes", "Tréboles", "Picas"};
             String[] valores = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
@@ -27,7 +27,7 @@ public class Baraja {
             }
         }
 
-        
+        CreacioJokers(cartes , 2);
         Collections.shuffle(cartes);
     }
 
@@ -41,4 +41,14 @@ public class Baraja {
     public int getSize() {
         return cartes.size();
     }
+
+    private void CreacioJokers(ArrayList<Fitxa> array, int numJ){
+         String color = "Lila";
+         String Valor = "Jocker";
+
+        for (int i = 0; i < numJ; i++) {
+            array.add(new Fitxa(color,Valor));
+        }
+    }
+
 }
